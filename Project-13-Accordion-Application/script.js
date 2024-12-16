e1eaa01142d618bae6aa4e910828213fc1bf21bb
theyogesh10 <yogeshpahade10@gmail.com>
2024-12-16 23:05:54 +0530

    Accordion Application

---
+++ Project-13-Accordion-Application/script.js
@@ -0,0 +1,7 @@
+const accordions = document.querySelectorAll('.accordion');
+
+accordions.forEach((accordion) => {
+    accordion.addEventListener('click', () => {
+        accordion.classList.toggle("active");
+    });
+});
