bcdc061d0562195918d76f636087dfe8b263d8a2
theyogesh10 <yogeshpahade10@gmail.com>
2024-12-17 23:51:11 +0530

    Telephone Formatter Application

---
+++ Project-14-Telephone-Formatter-Application/script.js
@@ -0,0 +1,18 @@
+
+const input = document.querySelector("input");
+
+let firstThreeNumbers = "";
+let previousValue = "";
+
+input.addEventListener("input", (e) => {
+
+    const inputValue = e.target.value.replace(/\D/g, '');
+
+    if (inputValue.length === 4 && previousValue.length < inputValue.length) {
+        firstThreeNumbers = inputValue.substring(0, 3);
+        input.value = `+(${firstThreeNumbers}) - ${inputValue[inputValue.length - 1]}`;
+    }
+
+    previousValue = inputValue;
+
+});
